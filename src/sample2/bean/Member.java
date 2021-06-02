a3201413f0467aa7a0c8a03cc69917278cb92b6b
dailydevp <hyde8547@naver.com>
2021-06-02 21:55:08 +0900

    first

---
+++ src/sample2/bean/Member.java
@@ -0,0 +1,44 @@
+package sample2.bean;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Member {
+//	field 작성하기
+	private String id;
+	private String name;
+	private String password;
+	private Date birth;
+	private Timestamp inserted;
+	
+	public Timestamp getInserted() {
+		return inserted;
+	}
+	public void setInserted(Timestamp inserted) {
+		this.inserted = inserted;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+}
